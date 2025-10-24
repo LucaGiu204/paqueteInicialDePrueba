12e4b080c2ad84e1c7e8a1936d61cdfd41351a81
LucaGiu204 <lucagiu204@gmail.com>
2025-10-24 10:21:06 -0300

    documentacion en suma

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,18 @@
+#' Suma dos números
+#'
+#' Suma dos números que dében ser positivos
+#'
+#' @param x Un valor o vector numerico
+#' @param y Un valor o vector numerico
+#'
+#' @returns La suma de x e y
+#'
+#'
+#' @examples
+#' suma(5, 15)
+#' suma(2, 3)
+#'
+#' @export
 suma <- function(x=10, y=20) {
   if (!is.numeric(x) || !is.numeric(y)) {
     stop("los argumentos deben ser numéricos")
