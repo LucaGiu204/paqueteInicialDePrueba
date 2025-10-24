685b89f69c8965b1dcd9f4174f9ddb8596f5837b
LucaGiu204 <lucagiu204@gmail.com>
2025-10-24 10:22:10 -0300

    agregao documentacion a funciones

---
+++ R/conversor.R
@@ -0,0 +1,16 @@
+#' convierte centimetros a pulgadas
+#'
+#' @description toma como entrada un valor o vector numerico en centimetros y devuelve su equivalente en pulgadas
+#'
+#' @param centimetros Un valor o vector numerico en centimetros
+#'
+#' @returns un valor o vector numerico en pulgadas
+#'
+#' @export
+#'
+#' @examples centimetros_a_pulgadas(10) # devuelve 3.937008
+#'
+centimetros_a_pulgadas <- function(centimetros) {
+  pulgadas <- centimetros / 2.54
+  return(pulgadas)
+}
